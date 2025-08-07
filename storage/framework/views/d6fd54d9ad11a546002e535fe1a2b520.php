6076fd99bcd8e213f6bddf1d4ef702d0dadd2c72
= <=>
2025-08-07 19:46:03 +0000

    synchronization damfeu branch commit

---
+++ storage/framework/views/d6fd54d9ad11a546002e535fe1a2b520.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php echo $__env->yieldContent('css'); ?>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo e(asset('assets/images/Logo.png')); ?>">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/bootstrap.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/fontawesome.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/venobox.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/animate.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/keyframe-animation.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/daterangepicker.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/magnific-popup.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/odometer.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/nice-select.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/swiper.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/slick.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/main/css/main.css')); ?>">
+    
+    <!-- CSS Simulateur de Prêt -->
+    <link rel="stylesheet" href="<?php echo e(asset('assets/css/loan-simulator.css')); ?>">
+    
+    <!-- CSS personnalisé pour le header -->
+    <link rel="stylesheet" href="<?php echo e(asset('assets/css/custom-header.css')); ?>">
+
+    <!-- SweetAlert2 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <title>COCEC</title>
+</head>
+
+<body>
+    <?php echo $__env->yieldContent('content'); ?>
+
+    <script src="<?php echo e(asset('assets/main/js/vendor/jquary-3.6.0.min.js')); ?>"></script>
+
+    <?php echo $__env->yieldContent('js'); ?>
+
+    <script src="<?php echo e(asset('assets/main/js/vendor/bootstrap-bundle.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/imagesloaded-pkgd.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/waypoints.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/venobox.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/odometer.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/meanmenu.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/smooth-scroll.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/jquery.isotope.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/magnific-popup.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/wow.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/swiper.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/slick.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/moment.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/daterangepicker.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/split-type.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/gsap.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/scroll-trigger.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/vendor/nice-select.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/contact.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/main/js/main.js')); ?>"></script>
+    
+    <!-- Script personnalisé pour le header responsive -->
+    <script src="<?php echo e(asset('assets/js/header-responsive.js')); ?>"></script>
+
+    <!-- SweetAlert2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    
+
+</body>
+
+</html><?php /**PATH C:\Users\user\Desktop\Microfinance\COCEC\resources\views/layout/main.blade.php ENDPATH**/ ?>
