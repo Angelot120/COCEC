89af939d93a7f5c6a902b7ba699bbb19fafb77b6
ADODODJI Kokouvi Damaz <adododji7@gmail.com>
2025-08-28 06:19:07 +0000

    Fix: ignore storage logs and views

--- storage/framework/views/7e24934d46ef91ddde584c4c3b6d3cd8.php
+++
@@ -1,5 +0,0 @@
-<div id="preloader">
-    <div class="loading" data-loading-text="COCEC" style="--bz-color-theme-primary: #EC281C"></div>
-    <div style="position: absolute; bottom: 50px; left: 50%; transform: translateX(-50%); text-align: center; color: #EC281C; font-weight: 700; font-size: 1.5rem;">COCEC, la microfinance autrement</div>
-</div>
-<!-- ./ preloader --><?php /**PATH D:\COCEC\resources\views/includes/main/loading.blade.php ENDPATH**/ ?>
